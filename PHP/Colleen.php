99841607ab34778db279d2df15b81961c459c7b6
Maxime FORTIN <m.fortin@dbmail.com>
2017-11-30 16:07:26 +0100

    Update Colleen.php

--- PHP/Colleen.php
+++ PHP/Colleen.php
@@ -1,18 +1,18 @@
 <?php
-//
-//	Comment outside the func.
-//
+/*
+	ONE COMMENT
+*/
 function foo()
 {
-	//
-	//	Comment inside the func.
-	//
-	$str = "<?php%c//%c//%cComment outside the func.%c//%cfunction foo()%c{%c%c//%c%c//%cComment inside the func.%c%c//%c%c%cstr = %c%s%c;%c%c%cing = sprintf(%cstr,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,36,34,%cstr,34,10,9,36,36,36,10,9,36,10,10,10,10,9,10,10,10,10);%c%cecho %cing;%c}%cfunction main()%c{%c%cfoo();%c}%cmain();%c?>%c";
-	$ing = sprintf($str,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,36,34,$str,34,10,9,36,36,36,10,9,36,10,10,10,10,9,10,10,10,10);
-	echo $ing;
+	$str = "<?php%c/*%c%cONE COMMENT%c*/%cfunction foo()%c{%c%c%cstr = %c%s%c;%c%c%cout = sprintf(%cstr,10,10,9,10,10,10,10,9,36,34,%cstr,34,10,9,36,36,36,10,9,36,10,10,10,10,10,9,10,10,9,10,10,10,10);%c%cecho %cout;%c}%cfunction main()%c{%c/*%c%cSECOND COMMENT%c*/%c%cfoo();%c}%cmain();%c?>%c";
+	$out = sprintf($str,10,10,9,10,10,10,10,9,36,34,$str,34,10,9,36,36,36,10,9,36,10,10,10,10,10,9,10,10,9,10,10,10,10);
+	echo $out;
 }
 function main()
 {
+/*
+	SECOND COMMENT
+*/
 	foo();
 }
 main();
